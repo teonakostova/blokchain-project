9d41d97c96e0d8eb95c41e4bb5d5912f291b40d2
kostovat <teona.kostova@loka.com>
2022-06-23 18:31:25 +0200

    initial commit

---
+++ smart-contracts/contracts/common/Ownable.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.18;
+
+
+contract Ownable {
+    address public owner;
+
+    event LogOwnershipTransfered(address indexed _currentOwner, address indexed _newOwner);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function Ownable() public {
+        owner = msg.sender;
+    }
+}
---
+++ smart-contracts/truffle-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  compilers: {
+    solc: {
+      version: "^0.4.18"
+    }
+  },
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "1655815099165"
+    }
+  }
+};
